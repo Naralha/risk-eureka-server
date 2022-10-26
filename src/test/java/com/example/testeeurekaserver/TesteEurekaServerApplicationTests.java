f13b45dd64fd283e3ee2094cf9f1705166bac8ac
vitor.rodrigues <vitor.rodrigues>
2022-10-26 21:52:35 +0100

    first commit

---
+++ src/test/java/com/example/testeeurekaserver/TesteEurekaServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.testeeurekaserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TesteEurekaServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
